fe167b802c682e889c76465ac49e729c1f8686f5
Israel Valverde <israelpereiravalverde@gmail.com>
2019-05-19 21:19:02 +0100

    Update build script to generate nuget pkg

--- build.ps1
+++ build.ps1
@@ -4,8 +4,33 @@ function Invoke-CommandWithLog {
     Write-Host "Starting $CommandName process" -ForegroundColor Yellow
     Invoke-Expression $Command
     Write-Host "$CommandName finished" -ForegroundColor Yellow
+    Write-Host ""
 }
 
-Invoke-CommandWithLog -Command "dotnet build src/PipelineNet.sln -c Release" -CommandName "build"
+$mainProjectPath = "src/PipelineNet/PipelineNet.csproj"
+$testProjectPath = "src/PipelineNet.Tests/PipelineNet.Tests.csproj"
+$solutionPath = "src/PipelineNet.sln"
 
-Invoke-CommandWithLog -Command "dotnet test src/PipelineNet.sln -c Release" -CommandName "test"
+$commitMessage = $null
+$packageVersion = $null
+
+if ($env:APPVEYOR_REPO_TAG -eq "true") {
+
+    $packageVersion = $env:APPVEYOR_REPO_TAG_NAME
+    Write-Host "Git version tag detected: '$packageVersion'"
+
+    $commitMessage = $env:APPVEYOR_REPO_COMMIT_MESSAGE
+    if (-not [string]::IsNullOrWhiteSpace($env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED)) {
+        $commitMessage = $commitMessage + "`n" + $env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED
+    }
+}
+
+
+Invoke-CommandWithLog -Command "dotnet build $solutionPath -p:Version=$packageVersion -c Release" -CommandName "build"
+
+Invoke-CommandWithLog -Command "dotnet test $testProjectPath -c Release --no-build" -CommandName "test"
+
+
+if ($null -ne $packageVersion) {
+    Invoke-CommandWithLog -Command "dotnet pack $mainProjectPath --no-build -p:Version=$packageVersion -c Release --include-symbols -o artifacts -p:PackageReleaseNotes=`"$commitMessage`"" -CommandName "pack"
+}
