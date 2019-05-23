05c8ceaf8632667298eed63adedd9f535760994e
Israel Valverde <israelpereiravalverde@gmail.com>
2019-05-23 21:43:08 +0100

    Automatically generate version on build

--- build.ps1
+++ build.ps1
@@ -3,21 +3,22 @@ function Invoke-CommandWithLog {
     Write-Host "----------" -ForegroundColor Yellow
     Write-Host "Starting $CommandName process" -ForegroundColor Yellow
     Invoke-Expression $Command
-    Write-Host "$CommandName finished" -ForegroundColor Yellow
-    Write-Host ""
+    Write-Host "$CommandName finished`n" -ForegroundColor Yellow
 }
 
+$nugetSourceUrl = "https://www.myget.org/F/pipelinenet/api/v2/package"
 $mainProjectPath = "src/PipelineNet/PipelineNet.csproj"
 $testProjectPath = "src/PipelineNet.Tests/PipelineNet.Tests.csproj"
 $solutionPath = "src/PipelineNet.sln"
 
 $commitMessage = $null
-$packageVersion = $null
+$packageVersionCommandArgument = [string]::Empty
 
 if ($env:APPVEYOR_REPO_TAG -eq "true") {
 
-    $packageVersion = $env:APPVEYOR_REPO_TAG_NAME
-    Write-Host "Git version tag detected: '$packageVersion'"
+    Write-Host "Git version tag detected: '$env:APPVEYOR_REPO_TAG_NAME'"
+
+    $packageVersionCommandArgument = " -p:Version=$packageVersion"
 
     $commitMessage = $env:APPVEYOR_REPO_COMMIT_MESSAGE
     if (-not [string]::IsNullOrWhiteSpace($env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED)) {
@@ -26,11 +27,13 @@ if ($env:APPVEYOR_REPO_TAG -eq "true") {
 }
 
 
-Invoke-CommandWithLog -Command "dotnet build $solutionPath -p:Version=$packageVersion -c Release" -CommandName "build"
+Invoke-CommandWithLog -Command "dotnet build $solutionPath -c Release$packageVersionCommandArgument" -CommandName "build"
 
 Invoke-CommandWithLog -Command "dotnet test $testProjectPath -c Release --no-build" -CommandName "test"
 
 
-if ($null -ne $packageVersion) {
-    Invoke-CommandWithLog -Command "dotnet pack $mainProjectPath --no-build -p:Version=$packageVersion -c Release --include-symbols -o artifacts -p:PackageReleaseNotes=`"$commitMessage`"" -CommandName "pack"
+if (-not [string]::IsNullOrWhiteSpace($packageVersionCommandArgument)) {
+    Invoke-CommandWithLog -Command "dotnet pack $mainProjectPath --no-build -c Release --include-symbols -o artifacts -p:PackageReleaseNotes=`"$commitMessage`"$packageVersionCommandArgument" -CommandName "pack"
+
+    Invoke-CommandWithLog -Command "dotnet nuget push *.nupkg -s $nugetSourceUrl -k $env:MYGET_KEY" -CommandName "publish"
 }
