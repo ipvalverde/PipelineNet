1836b6e7841f9711f975d528039d7ee4f79f1398
Israel Valverde <israelpereiravalverde@gmail.com>
2024-10-13 21:27:11 +0100

    Add using for finally interfaces

--- src/PipelineNet.ServiceProvider/ServiceCollectionExtensions.cs
+++ src/PipelineNet.ServiceProvider/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using PipelineNet.Finally;
 using PipelineNet.Middleware;
 using System;
 using System.Collections.Generic;
